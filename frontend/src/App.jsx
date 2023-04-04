0c6fe8ab7e87c280c1aa5da9856307e9ef95b589
Olli Toivanen <olliteed@gmail.com>
2023-04-04 18:16:12 +0300

    Display map

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,24 +1,23 @@
 import {
-  MapContainer, TileLayer, Marker, Popup,
+  MapContainer, TileLayer,
 } from 'react-leaflet';
 import React from 'react';
 
 function App() {
   return (
-    <MapContainer center={[51.505, -0.09]} zoom={13} scrollWheelZoom={false}>
+    <MapContainer
+      style={{
+        width: '100%',
+        height: '100vh',
+      }}
+      center={[51.505, -0.09]}
+      zoom={13}
+      scrollWheelZoom={false}
+    >
       <TileLayer
         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
       />
-      <Marker position={[51.505, -0.09]}>
-        <Popup>
-          A pretty CSS6 popup.
-          {' '}
-          <br />
-          {' '}
-          Easily customizable.
-        </Popup>
-      </Marker>
     </MapContainer>
   );
 }
