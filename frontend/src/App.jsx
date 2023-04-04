ea72c8623a17b80b1bc4692c6abd0be50c84d3ce
Olli Toivanen <olliteed@gmail.com>
2023-04-04 18:34:02 +0300

    Create Map-component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,24 +1,9 @@
-import {
-  MapContainer, TileLayer,
-} from 'react-leaflet';
 import React from 'react';
+import Map from './components/Map';
 
 function App() {
   return (
-    <MapContainer
-      style={{
-        width: '100%',
-        height: '100vh',
-      }}
-      center={[51.505, -0.09]}
-      zoom={13}
-      scrollWheelZoom={false}
-    >
-      <TileLayer
-        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-      />
-    </MapContainer>
+    <Map />
   );
 }
 
