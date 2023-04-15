1653c759467e5792397524c9d6824298f7237bae
Olli Toivanen <olliteed@gmail.com>
2023-04-15 14:25:48 +0300

    Add calculate button

--- frontend/src/components/BaseMap.jsx
+++ frontend/src/components/BaseMap.jsx
@@ -3,27 +3,34 @@ import {
   TileLayer, MapContainer,
 } from 'react-leaflet';
 import ClickLayer from './ClickLayer';
+import Info from './Info';
 
 function BaseMap() {
   useEffect(() => {
   }, []);
 
   return (
-    <MapContainer
-      style={{
-        width: '100%',
-        height: '100vh',
-      }}
-      center={[51.505, -0.09]}
-      zoom={13}
-      scrollWheelZoom={false}
-    >
-      <ClickLayer />
-      <TileLayer
-        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-      />
-    </MapContainer>
+    <>
+      <Info />
+
+      <MapContainer
+        style={{
+          width: '100%',
+          height: '100vh',
+        }}
+        center={[27, 60]}
+        zoom={4}
+        scrollWheelZoom
+      >
+
+        <ClickLayer />
+        <TileLayer
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        />
+      </MapContainer>
+
+    </>
   );
 }
 
--- frontend/src/components/ClickLayer.jsx
+++ frontend/src/components/ClickLayer.jsx
@@ -1,21 +1,26 @@
 import React, { useState } from 'react';
 import {
-  Marker, useMapEvents,
+  Marker, useMapEvents, Polyline,
 } from 'react-leaflet';
 
 function ClickLayer() {
   const [markers, setMarkers] = useState([]);
 
+  const limeOptions = { color: 'blue' };
+
   useMapEvents({
     click(e) {
       const newMarkers = markers.concat(e.latlng);
+      console.log(markers);
       setMarkers(newMarkers);
     },
 
   });
   return (
     <>
-      {markers.map((marker) => (<Marker position={marker} />))}
+      {markers.map((marker) => (<Marker key={marker} position={marker} />))}
+      <Polyline pathOptions={limeOptions} positions={markers} />
+
     </>
 
   );
