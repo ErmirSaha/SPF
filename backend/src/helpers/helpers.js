3e1d662aaead3829810fb73803be500aa2785972
Ermir Sahatciu <ermsah@utu.fi>
2023-04-20 14:21:03 +0300

    eslint fix

--- backend/src/helpers/helpers.js
+++ backend/src/helpers/helpers.js
@@ -42,8 +42,8 @@ function calculateDistance(locations, path) {
 }
 
 /* Dijkstra tänne */
+
 function TSP(locations) {
-  let counter = 0;
   let shortestPath = null;
   let shortestDistance = Infinity;
   const permute = (arr, m = []) => {
@@ -53,8 +53,6 @@ function TSP(locations) {
         shortestDistance = distance;
         shortestPath = m;
       }
-      // eslint-disable-next-line no-plusplus
-      counter++;
     } else {
       // eslint-disable-next-line no-plusplus
       for (let i = 0; i < arr.length; i++) {
@@ -70,8 +68,20 @@ function TSP(locations) {
   return {
     path: shortestPath,
     distance: shortestDistance,
-    counter,
   };
 }
 
-module.exports = { TSP };
+// esimerkki sijainnin
+const locations = [
+  { lat: 37.7749, lng: -122.4194 },
+  { lat: 40.7128, lng: -74.0060 },
+  { lat: 51.5074, lng: -0.1278 },
+  { lat: 35.6895, lng: 139.6917 },
+  // ... lisää enemmän
+];
+const result = TSP(locations);
+
+console.log('Backend');
+console.log(result);
+
+module.exports = TSP;
